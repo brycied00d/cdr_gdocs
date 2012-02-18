ef87ad5710238082f8b6e280263ec5352520862f
Bryce Chidester <bryce@cobryce.com>
2012-02-17 19:21:10 -0800

    New version with a split daemon and message queue.
    cdr2.php is a drop-in replacement for cdr.php. It will stash the row data to be inserted in a message queue.
    The daemon will receieve the queued message(s) and attempt to post it. If Google has a fault, the daemon will
    retry several times before putting the message back in the queue and reinitializing the GDocs connection.

---
+++ cdr2.php
@@ -0,0 +1,36 @@
+#!/usr/bin/php
+<?php
+error_reporting(E_ALL);
+ini_set('display_errors', 'false');
+ini_set('log_errors', 'true');
+ini_set('track_errors', 'true');
+ini_set('error_log', '/tmp/cdr_gdocs2.log');
+function do_log($data) { return file_put_contents(ini_get('error_log'), trim($data)."\n", FILE_APPEND); }
+
+do_log("CDR_GDOCS2 started");
+do_log("Environment: ".print_r($_ENV, true));
+do_log("Server: ".print_r($_SERVER, true));
+
+// Parse args -- $1 name, $2 number, $3 dest? (if picked up)
+$CIDNAME=$_SERVER['argv'][1];
+$CIDNUM=$_SERVER['argv'][2];
+do_log("Name: $CIDNAME Number: $CIDNUM");
+
+$queue = msg_get_queue( 42 /*ftok?*/);
+if(!$queue)
+{
+	do_log("Error while connecting to IPC message queue: $php_errormsg");
+	die();
+}
+
+$message = array('date'=>date('m/d/Y H:i:s', $_SERVER['REQUEST_TIME']+3600), 'customer'=>$CIDNAME, 'phone'=>$CIDNUM);
+do_log("Queueing message=".print_r($message, true));
+
+if(!msg_send($queue, 1, $message))
+{
+	do_log("Error while stuffing the IPC message queue: $php_errormsg");
+	die();
+}
+
+do_log("CDR_GDOCS2 complete. Exit.");
+?>
---
+++ cdr_gdocs_daemon.php
@@ -0,0 +1,210 @@
+#!/usr/bin/php
+<?php
+//Set the ticks
+declare(ticks = 1);
+ 
+//Fork the current process
+$processID = pcntl_fork();
+ 
+//Check to make sure the forked ok.
+if ( $processID == -1 )
+	echo "\n Error:  The process failed to fork. \n";
+else if ( $processID )	//This is the parent process.
+	exit;
+
+//We're now in the child process.
+
+//Now, we detach from the terminal window, so that we stay alive when it is closed.
+if ( posix_setsid() == -1 ) {
+	echo "\n Error: Unable to detach from the terminal window. \n";
+}
+ 
+//Get out process id now that we've detached from the window.
+$posixProcessID = posix_getpid();
+
+//Create a new file with the process id in it.
+file_put_contents("/var/run/cdr_gdocs_daemon.pid", $posixProcessID);
+
+function sig_handler($signo){
+	global $pids,$pidFileWritten;
+	switch($signo)
+	{
+		case SIGTERM:
+		case SIGHUP:
+		case SIGINT:
+			exit();
+			break;
+	}
+}
+// setup signal handlers to actually catch and direct the signals
+pcntl_signal(SIGTERM, "sig_handler");
+pcntl_signal(SIGHUP,  "sig_handler");
+pcntl_signal(SIGINT, "sig_handler");
+
+
+
+error_reporting(E_ALL);
+ini_set('display_errors', 'false');
+ini_set('log_errors', 'true');
+ini_set('track_errors', 'true');
+ini_set('error_log', '/tmp/cdr_gdocs_daemon.log');
+function do_log($data) { return file_put_contents(ini_get('error_log'), trim($data)."\n", FILE_APPEND); }
+
+do_log("CDR_GDOCS_DAEMON started");
+do_log("Environment: ".print_r($_ENV, true));
+do_log("Server: ".print_r($_SERVER, true));
+
+
+
+// Settings
+$email = 'brycec@qturbo.com';
+$password = 'rhtwlecsfdqofcbx';
+$spreadsheet = 'MEDITAB CALL LOG';
+$worksheet = 'Log';
+
+do_log("Configured $email : $password / $spreadsheet / $worksheet");
+
+set_include_path(get_include_path() . PATH_SEPARATOR . dirname(__FILE__) . '/ZendGdata-1.11.11/library/');
+do_log("include_path updated");
+
+
+$queue = msg_get_queue( 42 /*ftok?*/);
+if(!$queue)
+{
+	do_log("Error while connecting to IPC message queue: $php_errormsg");
+	die();
+}
+
+require_once 'Zend/Loader.php';	// Overall loader
+Zend_Loader::loadClass('Zend_Gdata');	// Overall Gdata class
+Zend_Loader::loadClass('Zend_Http_Client');	// For all HTTP communications
+Zend_Loader::loadClass('Zend_Gdata_ClientLogin');	// For username/pass login
+Zend_Loader::loadClass('Zend_Gdata_App_AuthException');	// For username/pass failure
+Zend_Loader::loadClass('Zend_Gdata_Spreadsheets');	// For the spreadsheet itself
+do_log("Zend libraries loaded.");
+
+
+$gdClient = retry(3, 30, 'gdata_auth', $email, $password);
+$ss_id = retry(3, 30, 'load_spreadsheet', $gdClient, $spreadsheet);
+$ws_id = retry(3, 30, 'load_worksheet', $gdClient, $ss_id, $worksheet);
+
+while(true)
+{
+	$msgtype=null;
+	$message=null;
+	if(!msg_receive($queue, 0, $msgtype, 1e6, $message))
+	{
+		do_log("huh, odd... msg_receive failed. Going to try again.");
+		sleep(5);
+		continue;
+	}
+	do_log("Processing message($msgtype)=".print_r($message, true));
+	// try... and upon failure, re-queue
+	$row_id = retry(5, 30, 'insert_row', $gdClient, $ss_id, $ws_id, $message);
+	if($row_id === false)
+	{
+		do_log("Seems like Google is being a prick. Re-queue message. Sleep. Refresh login.");
+		msg_send($queue, $msgtype+1, $message);
+		sleep(15);
+		$gdClient = retry(3, 30, 'gdata_auth', $email, $password);
+		$ss_id = retry(3, 30, 'load_spreadsheet', $gdClient, $spreadsheet);
+		$ws_id = retry(3, 30, 'load_worksheet', $gdClient, $ss_id, $worksheet);
+	}
+	sleep(1);
+}
+
+
+do_log("CDR_GDOCS_DAEMON complete. Exit.");
+
+
+
+function retry()
+{
+	$args = func_get_args();
+	$tries = array_shift($args);
+	$timeout = array_shift($args);
+	$name = array_shift($args);
+	$start = microtime(true);
+	for($i=0; $i<$tries; $i++)
+	{
+		if($timeout && (microtime(true)-$start) > $timeout)
+			return false;
+		$res = call_user_func_array($name, $args);
+		if($res !== false)
+			return $res;
+		do_log("Retry($name): i=$i failed.");
+		sleep(1);
+	}
+	return false;
+}
+
+function gdata_auth($email, $password)
+{
+	// Login to Google
+	try {
+		$client = Zend_Gdata_ClientLogin::getHttpClient($email, $password,
+			Zend_Gdata_Spreadsheets::AUTH_SERVICE_NAME);
+	} catch (Zend_Gdata_App_AuthException $ae) {
+		do_log("Unable to login to Google: ". $ae->getMessage());
+		//exit("Error: ". $ae->getMessage() ."\nCredentials provided were email: [$email] and password [$password].\n");
+		return false;
+	}
+	$gdClient = new Zend_Gdata_Spreadsheets($client, "QTurbo-cdr_gdocs-1.0");
+	do_log("Connected to Google.");
+	return $gdClient;
+}
+
+function load_spreadsheet($gdClient, $spreadsheet)
+{
+	// Open the spreadsheet
+	$spreadsheet_query = new Zend_Gdata_Spreadsheets_DocumentQuery();
+	$spreadsheet_query->setTitleExact($spreadsheet);
+	$spreadsheet_query->setDocumentType('spreadsheets');
+	try {
+		$spreadsheet_feed = $gdClient->getSpreadsheetFeed($spreadsheet_query);
+	} catch (Zend_Gdata_App_Exception $e) {
+		do_log("Zend_Gdata_App_Exception: ". $e->getMessage() . "\n" . $e->getTraceAsString());
+		//exit("Error: Zend_Gdata_App_Exception: ". $e->getMessage()."\n");
+		return false;
+	}
+	$spreadsheet_id = explode('/', $spreadsheet_feed->entries[0]->id->text);
+	$spreadsheet_id = $spreadsheet_id[7];
+	#echo "Spreadsheet ID: {$spreadsheet_id}\n";
+	do_log("Spreadsheet loaded: $spreadsheet_id");
+	return $spreadsheet_id;
+}
+
+function load_worksheet($gdClient, $spreadsheet_id, $worksheet)
+{
+	// Open the worksheet
+	$worksheet_query = new Zend_Gdata_Spreadsheets_DocumentQuery();
+	$worksheet_query->setTitleExact($worksheet);
+	$worksheet_query->setDocumentType('worksheets');
+	$worksheet_query->setSpreadsheetKey($spreadsheet_id);
+	try {
+		$worksheet_feed = $gdClient->getSpreadsheetFeed($worksheet_query);
+	} catch (Zend_Gdata_App_Exception $e) {
+		do_log("Zend_Gdata_App_Exception: ". $e->getMessage() . "\n" . $e->getTraceAsString());
+		//exit("Error: Zend_Gdata_App_Exception: ". $e->getMessage()."\n");
+		return false;
+	}
+	$worksheet_id = explode('/', $worksheet_feed->entries[0]->id->text);
+	$worksheet_id = $worksheet_id[8];
+	#echo "Worksheet ID: {$worksheet_id}\n";
+	do_log("Worksheet loaded: $worksheet_id");
+	return $worksheet_id;
+}
+
+function insert_row($gdClient, $spreadsheet_id, $worksheet_id, $rowData)
+{
+	try {
+		$res = $gdClient->insertRow($rowData, $spreadsheet_id, $worksheet_id);
+	} catch (Zend_Gdata_App_Exception $e) {
+		do_log("Zend_Gdata_App_Exception: ". $e->getMessage() . "\n" . $e->getTraceAsString());
+		//exit("Error: Zend_Gdata_App_Exception: ". $e->getMessage()."\n");
+		return false;
+	}
+	do_log("Row posted @ ".substr($res->getTitle()->getText(), 5).": $res");
+	return $res;
+}
+?>
